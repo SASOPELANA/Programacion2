9482aad91588d9821769bc25516a944f267c46da
SASOPELANA <sopekof@gmail.com>
2024-08-13 09:37:59 -0300

    13/04/24

---
+++ Unidad1/Punteros_Vectores/Ejemplo1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Los Punteros
+  // Son variables que almacenan direcciones de memoria.
+  // Son usados con frecuencia en lso programas en C para la asignacion de
+  // dinámica de memoria.
+
+  int var, *pint;
+
+  cout << var << endl;
+
+  cout << pint << endl;
+
+  cout << "Presione enter para continuar ....\n";
+  cin.get();
+  return 0;
+}
