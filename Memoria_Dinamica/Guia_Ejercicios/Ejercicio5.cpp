2a1a89bd436de713e809bbc6f47e76e0ed6b8269
SASOPELANA <sopekof@gmail.com>
2024-08-26 22:13:38 -0300

    Memoria dinamica - Ejercicio 5

---
+++ Memoria_Dinamica/Guia_Ejercicios/Ejercicio5.cpp
@@ -0,0 +1,14 @@
+/*
+  5_ Hacer una función que reciba un vector de enteros y su tamaño y luego
+  muestre el vector ordenado de forma ascendente, tener en cuenta que el vector
+  enviado como argumento no debe ser modificado.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Hola Mundo. " << endl;
+
+  return 0;
+}
