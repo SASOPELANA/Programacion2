5e55047974f2d79f17641324d3c99f2d8a9af58a
SASOPELANA <sopekof@gmail.com>
2024-08-31 01:54:39 -0300

    POO C++

---
+++ Programacion_Orientada_Objectos/Incrementador/Incrementador.cpp
@@ -0,0 +1,21 @@
+#include "Incrementador.h"
+// implementcion de los metodos de las clases
+Incrementador::Incrementador(int valorInicial, int valorIncremento){ // constructor
+    _cantidadIncrementos = 0;
+    _valorActual = valorInicial;
+    _valorIncremento = valorIncremento;
+}
+
+void Incrementador::incrementar(){
+  _valorActual += _valorIncremento;
+  _cantidadIncrementos++;
+}
+
+// Obtenemos el valor --> get
+int Incrementador::getValor(){
+  return _valorActual;
+}
+
+int Incrementador::getCantidadIncrementos(){
+  return _cantidadIncrementos;
+}
